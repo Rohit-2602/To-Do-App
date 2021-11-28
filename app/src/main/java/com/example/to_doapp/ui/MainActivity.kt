7cc5999f9b694a60ec2d0e96a7aeab03d131ff48
Rohit Sharma <65807152+Rohit-2602@users.noreply.github.com>
2021-11-28 14:25:16 +0530

    Update themes

--- app/src/main/java/com/example/to_doapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/to_doapp/ui/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.to_doapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import com.example.to_doapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,7 +9,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         setContentView(R.layout.activity_main)
     }
 }
